7c53d20f7780e7ccfd1cb50e645f308488e0b059
Goglicha <136063402+Goglicha@users.noreply.github.com>
2023-09-19 02:33:32 +0400

    Add files via upload

---
+++ homework_for_first_lesson.py
@@ -0,0 +1,7 @@
+name = "Nini"
+lastname = "Goglichadze"
+age = 25
+mail="randommail@gmail.com"
+number = 53638155633
+
+print (name + " " + lastname + " " + str(age) + " " + mail + " " + str(number))
