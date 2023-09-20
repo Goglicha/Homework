3cfdbff73685fc755689b4346ce0cf74eec85f5a
Goglicha <136063402+Goglicha@users.noreply.github.com>
2023-09-20 19:39:53 +0400

    Add files via upload

---
+++ les3/day3/day3.py
@@ -0,0 +1,8 @@
+name = "Nini"
+lastname = "Goglichadze"
+age = 25
+number = 56789132436454
+mail = "randommail@gmail.com"
+
+
+print (name + " " + lastname + " " + str(age) + " " + str(number) + " " + mail)
