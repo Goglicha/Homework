c761b00b0d948a2fecc32d22755aaf152a9bfe2a
Goglicha <136063402+Goglicha@users.noreply.github.com>
2023-09-27 21:33:15 +0400

    Add files via upload

---
+++ DAY-1/Homework/homework.py
@@ -0,0 +1,61 @@
+Keira = 2
+Patu = 28
+Nini = 25
+
+Keira += 5
+Patu += 5
+Nini += 5
+
+print (Keira)
+print(Patu)
+print(Nini)
+
+
+item = 12
+amount = 3
+total = item * amount
+print(total)
+
+
+item = 4
+amount = 4.6
+discount = 2.1
+total = amount * discount 
+print(total)
+
+
+city = "Milan"
+city = "London"
+city = "Tbilisi"
+print(city)
+
+Number1 = 23
+Number2 = 4
+print (Number1 + Number2)
+
+
+Numb3 = "34"
+Numb4 = "23"
+print(Numb3 + Numb4)
+
+name = "nini"
+age = "25" 
+print(name + age)
+
+
+result1 = ((2 + 4 )* 6 / 3 - 2 * (2 + 3 ) + 3.5)
+result2 = (((2 + 4 )* 6 / 3 - 2 * (2 + 3 ) + 3.5) // 5)
+result3 = (2 + 4 * 6 / 3 - 2 * 2 + 3 + 3.5 )
+result4 = ((2 + 4 * 6 / 3 - 2 * 2 + 3 + 3.5 ) // 3)
+print (result1)
+print (result2)
+print (result3)
+print (result4)
+
+itemName = "apple"
+itemCost = 1000 
+discount = 998 
+itemCostAfterDiscont = itemName + " " + (str(discount))
+print(itemCostAfterDiscont)
+
+
